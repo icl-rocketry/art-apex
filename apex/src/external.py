4d8bf3e7bb54c772b34856a5216685448946f882
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-07-06 12:02:05 +0100

    Added sensors skeleton

--- apex/src/external.py
+++ apex/src/external.py
@@ -1,13 +1,17 @@
 TEST_MODE = True
 if TEST_MODE:
     from unittest.mock import Mock  # Mock objects can do anything
-    
+    from time import time_ns
     I2C = Mock()
     UART = Mock()
     GPS_GtopI2C = Mock()
+    millis = lambda: time_ns() // 1_000_000
 else:
     from busio import I2C, UART
     from adafruit_gps import GPS_GtopI2C
+    from time import monotonic_ns
+    millis = lambda: monotonic_ns() // 1_000_000
+
 
 from time import sleep
 
