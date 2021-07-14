bdb03bfb139464d5572f89d605568d211b05ac08
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-07-14 01:41:30 +0100

    control onboard led

--- apex/src/state_machine.py
+++ apex/src/state_machine.py
@@ -10,6 +10,19 @@ from gps import gps
 from sms import sms
 from external import sleep_ms
 import struct
+import pwmio
+
+rled = pwmio.PWMOut(board.LED_R, frequency=440)
+gled = pwmio.PWMOut(board.LED_G, frequency=440)
+bled = pwmio.PWMOut(board.LED_B, frequency=440)
+
+def LED(r,g,b):
+    rduty = int(65535 -(65535 * r/255))
+    gduty = int(65535 -(65535 * g/255))
+    bduty = int(65535 -(65535 * b/255))
+    rled.duty_cycle = rduty
+    gled.duty_cycle = gduty
+    bled.duty_cycle = bduty
 
 class state:
     def run(self):
