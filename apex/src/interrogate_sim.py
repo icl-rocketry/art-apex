13112b3999c814c853d99c12b5151dfd06ebf9b7
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-07-15 17:58:23 +0100

    include texting functionality

--- apex/src/interrogate_sim.py
+++ apex/src/interrogate_sim.py
@@ -7,6 +7,9 @@ rled = pwmio.PWMOut(board.LED_R, frequency=440)
 gled = pwmio.PWMOut(board.LED_G, frequency=440)
 bled = pwmio.PWMOut(board.LED_B, frequency=440)
 
+with open("phone.number") as pno:
+    num = pno.readline()
+
 def LED(r,g,b):
     rduty = int(65535 -(65535 * r/255))
     gduty = int(65535 -(65535 * g/255))
@@ -29,7 +32,33 @@ def send(cmd: str) -> str:
         print(resp.decode("ascii"))
         return resp.decode("ascii")
 
+def send_msg(msg: str, num):
+    send("AT+CMGF=1") # Text message mode
+    send("AT+CMGS=\""+num+"\"")
+    send(msg)
+    sim.write(bytes(chr(26).encode("ascii")))
+
+        
+print("Checking Connection")
+send("at")
+print("Getting SIM number")
+send("at+ccid")
+print("Check registration")
+send("at+creg?")
+print("Check input voltage")
+send("at+cbc")
+print("sending message")
+
+num = "+447813580661"
+
 while True:
     inp = input("Command: ")
-    send(inp)
+    if inp == "text":
+        numin = input("number: ")
+        if numin != "":
+            num = numin
+        msg = input("message: ")
+        send_msg(msg, num)
+    else:
+        send(inp)
     time.sleep(1)
