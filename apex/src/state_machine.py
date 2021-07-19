02c39f2aae0ed5b3715dd6b474da4a7ca26b1dbe
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-07-19 13:36:26 +0100

    Added diagnostic stage and inverted dependencies

--- apex/src/state_machine.py
+++ apex/src/state_machine.py
@@ -29,35 +29,49 @@ def sleep_ms(ms):
 # FIXME these pins might be wrong
 i2c = I2C(scl=board.GP27, sda=board.GP26, frequency=100000, timeout=100000)
 
-sms = sms(rx=board.GP5, tx=board.GP4)
-sensors = sensors(i2c)
-gps = gps(i2c, 1000)
-
-
 class state:
     def run(self):
         raise NotImplementedError("Not implemented")
 
+class diagnostic(state):
+    def run(self):
+        sms_ = sms(rx=board.GP5, tx=board.GP4)
+        sms_.report()
+        sensors_ = sensors(i2c)
+        sms_.send_msg("sensors ok")
+        gps_ = gps(i2c, 1000)
+        sms_.send_msg("gps ok")
+
+        while sms_.recv_msg() != "ok":
+            sleep_ms(500)
+        return preflight(sms_, sensors_, gps_)
 
 class preflight(state):
+    def __init__(self, sms, sensors, gps):
+        self._sms = sms
+        self._sensors = sensors
+        self._gps = gps
+
     def run(self):
-        while "launch" not in sms.recv_msg():
+        while self._sms.recv_msg() != "launch":
             sleep_ms(500)
-        sms.send_msg("launching")
-        return flight()
+        self._sms.send_msg("launching")
+        return flight(self._sms, self._sensors, self._gps)
 
 
 class flight(state):
-
     # TODO optimise buffer size such that data can be flushed in the same amount of time that would be
-    # spent sleeping - Nasty hack to get uasyncio to work in circuitpython
+    # spent sleeping
     _recordings_before_flush = 3
     _buffer_size = _recordings_before_flush * sensors.data_size
     _capture_rate = 20
     _delay = 1000//_capture_rate
     _flight_time = 5 * 60 * _capture_rate  # 5 minutes of flight
 
-    def __init__(self):
+    def __init__(self, sms, sensors, gps):
+        self._sms = sms
+        self._sensors = sensors
+        self._gps = gps
         self._storage = open("log.bin", "wb", buffering=self._buffer_size)
 
     def run(self):
@@ -65,30 +79,34 @@ class flight(state):
         while i < self._flight_time:
             i += 1
             start = millis()
-            data = sensors.get()
+            data = self._sensors.get()
 
             for reading in data:
                 self._storage.write(struct.pack("f", reading))
 
             end = millis()
             sleep_ms(max(0, self._delay - (end - start)))
-        return postflight()
+        return postflight(self._sms, self._gps)
 
 
 class postflight(state):
+    def __init__(self, sms, gps):
+        self._sms = sms
+        self._gps = gps
+
     def run(self):
         resp = ""
         wait = 0
         while True:
-            resp = sms.recv_msg()
-            gps.update()
+            resp = self._sms.recv_msg()
+            self._gps.update()
 
             if resp == "siren":
                 speaker.siren()
 
             elif resp == "location":
-                msg = gps.create_msg()
-                sms.send_msg(msg)
+                msg = self._gps.create_msg()
+                self._sms.send_msg(msg)
 
             sleep(0.5)
 
