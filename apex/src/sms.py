98d7106fe0b71f91645c013fa659e200c1cb7492
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-07-19 13:28:13 +0100

    sms reporting is now a public method

--- apex/src/sms.py
+++ apex/src/sms.py
@@ -6,25 +6,16 @@ def sleep_ms(ms):
     sleep(ms//1000)
 
 
-with open("phone.number") as pno:
-    OWNER_NUMBER = pno.readline()
+with open("phone.number") as file:
+    OWNER_NUMBER = file.readline()
 
 print(OWNER_NUMBER)
 
 SEND_DELAY_MS = 50
 
-
 class sms:
     def __init__(self, rx, tx):
         self._uart = UART(baudrate=9600, rx=rx, tx=tx)
-
-        # Perform self check
-        if not self._check():
-            ...  # TODO replace this with a buzz or something
-
-        self._report()
-
-        # TODO indicate sim setup complete
 
     def _send(self, cmd: str) -> str:
         self._uart.write(bytes((cmd+"\r\n").encode("ascii")))
@@ -34,19 +25,13 @@ class sms:
             return ""
         return resp.decode("ascii")
 
-    def _check(self) -> bool:
-        if "ok" not in self._send("at"):
-            return False
-        if "error" in self._send("at+ccid"):
-            return False
-        if "error" in self._send("at+creg?"):
-            return False
-        return True
-
     # Report will send commands to the sim800l and collect the responses.
     # Then the responses will be sent via sms to the "owner"
-    def _report(self):
+    def report(self):
         rep = ""
+        rep += self._send("at")
+        rep += self._send("at+ccid")
+        rep += self._send("at+creg?")
         rep += self._send("at+csq")  # signal strength
         rep += self._send("AT+COPS?")  # connected network
         rep += self._send("AT+CBC")  # lipo state
@@ -56,7 +41,7 @@ class sms:
         self._send("AT+CMGF=1")  # Text message mode
         self._send(f"AT+CMGS=\"{OWNER_NUMBER}\"")
         self._send(msg)
-        self._uart.write(bytes(chr(26)))
+        self._uart.write(bytes(chr(26).encode("ascii")))
 
     def recv_msg(self) -> str:
         self._send("AT+CMGF=1")  # Text message mode
