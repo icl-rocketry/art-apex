f3e02103acc6b873bf3f2f73f913e0b3e27add95
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-08-19 18:00:40 +0100

    Added ahrs to rotate the rocket

--- apex/telemetry/send_dummy_data.py
+++ apex/telemetry/send_dummy_data.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import random
 
 with open("sample_data.csv", "r") as f:
     data = (list(map(float, i.split(","))) for i in f.readlines())
@@ -12,6 +13,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 
 sock.sendto(bytes("start", "ascii"), (IP, UDP_PORT))
 
+#Data has length 1207 - use -0.00025x(x-1207) to simulate the altitude
+alt = lambda x: -0.00025*x*(x-1207)
+i = 0
 for vals in data:
     arr = bytearray(45)
     struct.pack_into("b", arr, 0, 97)
@@ -25,7 +29,8 @@ for vals in data:
     struct.pack_into("f", arr, 1+28, vals[7])
     struct.pack_into("f", arr, 1+32, vals[8])
     struct.pack_into("f", arr, 1+36, vals[9])
-    struct.pack_into("f", arr, 1+40, 0)
+    struct.pack_into("f", arr, 1+40, alt(i) + random.uniform(-3, 3))
     sock.sendto(bytes(arr), (IP, UDP_PORT))
+    i += 1
 
 sock.sendto(bytes("end", "ascii"), (IP, UDP_PORT))
