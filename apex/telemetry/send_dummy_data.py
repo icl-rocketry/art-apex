e46e806697910fc0b1486a5e85471f4aaa9aea4e
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-08-19 17:45:40 +0100

    Added example data

--- apex/telemetry/send_dummy_data.py
+++ apex/telemetry/send_dummy_data.py
@@ -1,43 +1,31 @@
 import socket
 import struct
-import random
+
+with open("sample_data.csv", "r") as f:
+    data = (list(map(float, i.split(","))) for i in f.readlines())
+
 
 IP = "192.168.1.16"
 UDP_PORT = 8080
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 
 sock.sendto(bytes("start", "ascii"), (IP, UDP_PORT))
 
-time = 0
-altitude = 0
-for i in range(100):
-    time += random.uniform(45, 55)
-    accel_x = random.uniform(-8, 8)
-    accel_y = random.uniform(-8, 8)
-    accel_z = random.uniform(-8, 8)
-    gyro_x = random.uniform(-8, 8)
-    gyro_y = random.uniform(-8, 8)
-    gyro_z = random.uniform(-8, 8)
-    mag_x = random.uniform(-8, 8)
-    mag_y = random.uniform(-8, 8)
-    mag_z = random.uniform(-8, 8)
-    altitude += random.uniform(0, 2)
-
+for vals in data:
     arr = bytearray(45)
     struct.pack_into("b", arr, 0, 97)
-    struct.pack_into("f", arr, 1+0, time)
-    struct.pack_into("f", arr, 1+4, accel_x)
-    struct.pack_into("f", arr, 1+8, accel_y)
-    struct.pack_into("f", arr, 1+12, accel_z)
-    struct.pack_into("f", arr, 1+16, gyro_x)
-    struct.pack_into("f", arr, 1+20, gyro_y)
-    struct.pack_into("f", arr, 1+24, gyro_z)
-    struct.pack_into("f", arr, 1+28, mag_x)
-    struct.pack_into("f", arr, 1+32, mag_y)
-    struct.pack_into("f", arr, 1+36, mag_z)
-    struct.pack_into("f", arr, 1+40, altitude)
-    print(time, altitude)
+    struct.pack_into("f", arr, 1+0, vals[0])
+    struct.pack_into("f", arr, 1+4, vals[1])
+    struct.pack_into("f", arr, 1+8, vals[2])
+    struct.pack_into("f", arr, 1+12, vals[3])
+    struct.pack_into("f", arr, 1+16, vals[4])
+    struct.pack_into("f", arr, 1+20, vals[5])
+    struct.pack_into("f", arr, 1+24, vals[6])
+    struct.pack_into("f", arr, 1+28, vals[7])
+    struct.pack_into("f", arr, 1+32, vals[8])
+    struct.pack_into("f", arr, 1+36, vals[9])
+    struct.pack_into("f", arr, 1+40, 0)
     sock.sendto(bytes(arr), (IP, UDP_PORT))
 
 sock.sendto(bytes("end", "ascii"), (IP, UDP_PORT))
