1ddfa3e7e8e81724a4a7bd196fb65bba9c2b3ea8
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-08-19 12:55:04 +0100

    Moved chart code into separate file

---
+++ apex/telemetry/js/charts.js
@@ -0,0 +1,51 @@
+window.createChart = (id, xMin, xMax, yMin, yMax) => {
+    let ctx = document.getElementById(id).getContext('2d');
+    let data = [];
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: "X acceleration",
+                data: data,
+                radius: 0,
+                borderColor: '#FF0000',
+                tension: 0.01,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                xAxis: {
+                    max: xMax,
+                    min: xMin,
+                    grid: {
+                        drawBorder: false,
+                        display: false
+                    },
+                    type: "linear"
+                },
+                yAxis: {
+                    max: yMax,
+                    min: yMin,
+                    grid: {
+                        drawBorder: false,
+                        display: false
+                    }
+                },
+            },
+            plugins: {
+                legend: false
+            }
+        }
+    });
+
+    eval(`
+    window.update_${id} = (x, y) => {
+        data.push({
+            x: x,
+            y: y
+        });
+        myChart.update("none");
+    }
+    `);
+}
