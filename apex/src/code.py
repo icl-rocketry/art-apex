4944359e1c7bab56052c8db7177a210ebc3061c4
Akshat-Tripathi <akshat.tripathi6568@gmail.com>
2022-03-29 10:31:14 +0100

    Test commit

--- apex/src/code.py
+++ apex/src/code.py
@@ -2,14 +2,17 @@ from stateMachine import diagnostic
 from speaker import Speaker 
 from sensors import Sensors
 
-speaker = Speaker()
+# speaker = Speaker()
 sensors = Sensors()
-state = diagnostic(speaker, sensors) 
-try: 
-    while (state is not None):
-       state = state.run()
-except Exception as e:
-    file = open("error.txt", "w")
-    file.write(str(e))
-    file.close() 
-    raise e
+# state = diagnostic(speaker, sensors) 
+# try: 
+#     while (state is not None):
+#        state = state.run()
+# except Exception as e:
+#     file = open("error.txt", "w")
+#     file.write(str(e))
+#     file.close() 
+#     raise e
+
+for i in range(10):
+    print(sensors._getBarometer())
