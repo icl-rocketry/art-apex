aa93b4fc6f5d88fc683f94ff2e2b35f917f629e8
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-07-02 22:47:53 +0100

    Added headers to parsed files

--- apex/data/parser.py
+++ apex/data/parser.py
@@ -3,6 +3,7 @@ import struct
 import sys
 import os
 
+header = ["Time (ms)", "Acc_x", "Acc_y", "Acc_z", "Gyro_x", "Gyro_y", "Gyro_z", "Mag_x", "Mag_y", "Mag_z", "Lin_acc_x", "Lin_acc_y", "Lin_acc_z", "Geo_mag_i", "Geo_mag_j", "Geo_mag_k", "Geo_mag_w", "Rot_vec_i", "Rot_vec_j", "Rot_vec_k", "Rot_vec_w", "Pressure", "Temperature"]
 
 if __name__ == "__main__":
     path = sys.argv[1]
@@ -10,8 +11,10 @@ if __name__ == "__main__":
     with open(path, "rb") as file:
         records = list(struct.iter_unpack("Iffffffffffffffffffffff", file.read()))
 
+    records = [header] + records
+
     fname, _ = os.path.splitext(path)    
     with open(fname + "_parsed.csv", "w") as file:
         for record in records:
-            file.write(", ".join(map(str, record)))
+            file.write(", ".join(map(str, record)) + "\n")
 
