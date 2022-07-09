28003b46116994c373e7660b24bed06702dc0144
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-07-09 22:58:50 +0100

    Added wifi connection code

--- apex/src/apex/apex.ino
+++ apex/src/apex/apex.ino
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-
 /*
   Web client
 
@@ -24,88 +23,80 @@
 #include <WiFi.h>
 
 // Enter your WiFi SSID and password
-char ssid[] = "ASTRA3";             // your network SSID (name)
-char pass[] = "password";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                      // your network key Index number (needed only for WEP)
+char ssid[] = "ASTRA3";   // your network SSID (name)
+char pass[] = "password"; // your network password (use for WPA, or use as key for WEP)
+char start_message[] = "START?";
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-
-char server[] = "wifitest.adafruit.com";    // name address for adafruit test
-char path[]   = "/testwifi/index.html";
+IPAddress server(192, 168, 238, 238); // numeric IP for Google (no DNS)
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(115200);
+void setup()
+{
+    // Initialize serial and wait for port to open:
+    Serial.begin(115200);
 
-  // attempt to connect to Wifi network:
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
+    // attempt to connect to Wifi network:
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
 
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("Connected to WiFi");
-  printWifiStatus();
-
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.print("GET "); client.print(path); client.println(" HTTP/1.1");
-    client.print("Host: "); client.println(server);
-    client.println("Connection: close");
-    client.println();
-  }
-}
-
-void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true) {
-      delay(100);
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
     }
-  }
+
+    Serial.println("");
+    Serial.println("Connected to WiFi");
 }
 
+void loop()
+{
+    while (1)
+    {
+        Serial.println("\nStarting connection to server...");
+        // if you get a connection, report back via serial:
+        if (client.connect(server, 8080))
+        {
+            Serial.println("connected to server");
+            int n = client.write(start_message);
+            Serial.println(n);
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+            delay(100);
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+            char c = client.read();
+            Serial.println(c, HEX);
+            if (c == 'y')
+            {
+                Serial.println("YAY");
+                client.write("DONE");
+                client.stop();
+                goto end;
+            }
+        }
+        delay(1000);
+    }
+end:
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+    // if the server's disconnected, stop the client:
+    if (!client.connected())
+    {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
+
+        // do nothing forevermore:
+        while (true)
+        {
+            say_hi();
+            delay(100);
+        }
+    }
 }
