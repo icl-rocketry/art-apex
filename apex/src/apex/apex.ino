3da6aec6aae91116bd919d090e7372b860d8be7d
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-07-09 22:02:46 +0100

    Working wifi example

--- apex/src/apex/apex.ino
+++ apex/src/apex/apex.ino
@@ -1,75 +1,111 @@
-#include <Wire.h>
-#include <Adafruit_DPS310.h>
-#include <Adafruit_NeoPixel.h>
+// SPDX-FileCopyrightText: 2020 Brent Rubell for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
 
 
-#define NUMPIXELS 1
+/*
+  Web client
 
-Adafruit_DPS310 dps;
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+ This sketch connects to a website (wifitest.adafruit.com/testwifi/index.html)
+ using the WiFi module.
 
-void setup()
-{
-    Serial.begin(115200);
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the Wifi.begin() call accordingly.
 
-    Wire1.setPins(SDA1, SCL1);
-    Wire1.begin();
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the Wifi.begin() call accordingly.
 
-    if (!dps.begin_I2C(DPS310_I2CADDR_DEFAULT, &Wire1))
-    { // Can pass in I2C address here
-        while (1)
-        {
-            Serial.println("Failed to find DPS");
-            delay(10);
-        }
-    }
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+ */
 
-    dps.setMode(DPS310_CONT_PRESTEMP);
-    dps.configurePressure(DPS310_128HZ, DPS310_8SAMPLES);
-    dps.configureTemperature(DPS310_128HZ, DPS310_8SAMPLES);
+#include <WiFi.h>
 
-#if defined(NEOPIXEL_POWER)
-    // If this board has a power control pin, we must set it to output and high
-    // in order to enable the NeoPixels. We put this in an #if defined so it can
-    // be reused for other boards without compilation errors
-    pinMode(NEOPIXEL_POWER, OUTPUT);
-    digitalWrite(NEOPIXEL_POWER, HIGH);
-#endif
+// Enter your WiFi SSID and password
+char ssid[] = "ASTRA3";             // your network SSID (name)
+char pass[] = "password";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                      // your network key Index number (needed only for WEP)
 
-    pixels.begin();
-    pixels.setBrightness(20);
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 
-    Serial.println("Started");
+char server[] = "wifitest.adafruit.com";    // name address for adafruit test
+char path[]   = "/testwifi/index.html";
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.print("GET "); client.print(path); client.println(" HTTP/1.1");
+    client.print("Host: "); client.println(server);
+    client.println("Connection: close");
+    client.println();
+  }
 }
 
-void loop()
-{
-    sensors_event_t temp_event, pressure_event;
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
 
-      pixels.fill(0xFF0000);
-      pixels.show();
-      delay(500);
-      pixels.fill(0x00FF00);
-      pixels.show();
-      delay(500);
-      pixels.fill(0x0000FF);
-      pixels.show();
-      delay(500);
-
-
-    while (!dps.temperatureAvailable() || !dps.pressureAvailable())
-    {
-        return; // wait until there's something to read
-    }
-
-    dps.getEvents(&temp_event, &pressure_event);
-    Serial.print(F("Temperature = "));
-    Serial.print(temp_event.temperature);
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(pressure_event.pressure);
-    Serial.println(" hPa");
-
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
     Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true) {
+      delay(100);
+    }
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
