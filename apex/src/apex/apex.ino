9acdca4f13bd7aa53ec16ccacd7d2077f1a6c5d3
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-07-09 21:32:36 +0100

    Got basic i2c scanner working

---
+++ apex/src/apex/apex.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+
+  Wire1.setPins(SDA1, SCL1);
+  Wire1.begin();
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire1.beginTransmission(address);
+    error = Wire1.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);          
+}
