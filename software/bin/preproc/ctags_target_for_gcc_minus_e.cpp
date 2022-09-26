8673bf099dc4d882b66d0bd35d561111494b7df0
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-09-26 21:50:02 +0100

    Removed bin

--- software/bin/preproc/ctags_target_for_gcc_minus_e.cpp
+++
@@ -1,9 +0,0 @@
-# 1 "C:\\Users\\Akshat\\Documents\\icl-art\\art-apex\\apex\\src\\apex\\apex.ino"
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-  Serial.println("testing testing 123");
-  delay(100);
-}
