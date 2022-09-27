5a585fdcc70c85d04e465c9319170c81ed7b8ff0
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-09-27 16:21:37 +0100

    Added skeleton for zap

--- software/apex/zap/zapStream.hpp
+++ software/apex/zap/zapStream.hpp
@@ -1,17 +1,31 @@
 #pragma once
 
+#include "../util/uint.hpp"
+#include "../util/buffer.hpp"
+#include "../util/linked_list.hpp"
 #include "streams/module.hpp"
+#include "message.hpp"
 
 class ZapStream {
 public:
-    ZapStream(Stream_& stream): stream(stream) {};
+    ZapStream(Stream_& stream);
+
+    bool assume_role(Role role);
 
     template <typename T>
-    bool send(uint8_t msg_id, T msg);
-    
+    bool send_msg(Message<T> msg);
+
     template <typename T>
-    bool recv(uint8_t expected_msg_id, T* msg);
+    bool read_msg(uint32_t expected_msg_id, Message<T> msg);
+
+    // TODO: Maybe add something to read the first message in the buffer regardless of its contents?
 
 private:
     Stream_& stream;
+    uint32_t seq_num;
+    Role role;
+
+    // Reader specific members
+    Buffer* buffer; // Contains all the unprocessed data received from the stream
+    LinkedList<Message<void*>> messages; // All the messages
 };
