e128f6b188f796febf8337a2cc9ea78f0796b8eb
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-09-27 15:07:59 +0100

    Updated stream to return it's mtu

--- software/apex/zap/streams/serialStream.cpp
+++ software/apex/zap/streams/serialStream.cpp
@@ -7,3 +7,7 @@ bool SerialStream::send(const uint8_t* src, size_t len)  {
 bool SerialStream::recv(uint8_t* dest, size_t len)  {
     return len == serial.readBytes(dest, len); // We only care if everything was read
 }
+
+size_t SerialStream::get_max_bytes() {
+    return SERIAL_STREAM_MAX_BYTES;
+}
--- software/apex/zap/streams/serialStream.hpp
+++ software/apex/zap/streams/serialStream.hpp
@@ -3,10 +3,12 @@
 #include "module.hpp"
 #include <Arduino.h>
 
+#define SERIAL_STREAM_MAX_BYTES 64
 class SerialStream : public Stream_ {
     SerialStream(HardwareSerial& serial) : serial(serial) {};
 
     // Override functions from Stream_
+    virtual size_t get_max_bytes();
     virtual bool recv(uint8_t* dest, size_t len);
     virtual bool send(const uint8_t* src, size_t len);
 private:
