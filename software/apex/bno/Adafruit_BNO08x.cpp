274d02ad0deaea8b2c7ceb174967bc0b686bf36f
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-11-11 23:16:35 +0000

    smth dumb is happening

--- software/apex/bno/Adafruit_BNO08x.cpp
+++ software/apex/bno/Adafruit_BNO08x.cpp
@@ -108,16 +108,17 @@ bool Adafruit_BNO08x::begin_I2C(uint8_t i2c_address, TwoWire *wire,
     delete i2c_dev; // remove old interface
   }
 
-  auto i2c_dev1 = new Adafruit_I2CDevice(75, wire);
+  for (uint8_t addr = 0x00; addr <= 0x7F; addr++) {
+    auto i2c_dev1 = new Adafruit_I2CDevice(addr, &Wire1);
 
-  if (i2c_dev1->begin()) {
-    Serial.println(75);
-    Serial.println("Success");
+    if (i2c_dev1->begin()) {
+      Serial.println(addr);
+      Serial.println("Success");
+    }
+
+    delete i2c_dev1;
+    delay(50);
   }
-
-  delete i2c_dev1;
-  delay(50);
-
 
   i2c_dev = new Adafruit_I2CDevice(i2c_address, wire);
 
