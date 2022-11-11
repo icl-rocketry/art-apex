f2162d1a9b5ee68c077c34aab9f0576900cfe776
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-11-11 22:19:19 +0000

    Just scan everything

--- software/apex/apex.ino
+++ software/apex/apex.ino
@@ -11,17 +11,25 @@ void setup(void) {
 
   Serial.println("Adafruit BNO08x test!");
 
+  delay(100);
+
+  bool found = false;
+
   // Try to initialize!
-  if (!bno08x.begin_I2C(0x4D, &Wire1, 0)) {
-    // if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte
-    // UART buffer! if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    Serial.println("Failed to find BNO08x chip");
-    while (1) {
-      delay(10);
+  for (uint8_t x = 0; i < 0x100; i++) {
+    if (!bno08x.begin_I2C(0x4D, &Wire1, 0)) {
+      Serial.println("Failed to find BNO08x chip");
+      delay(50);
+    } else {
+      Serial.println(x);
+      found = true;
+      break;
     }
   }
-  Serial.println("BNO08x Found!");
 
+  if (found) {
+    Serial.println("BNO08x Found!");
+  }
   for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
     Serial.print("Part ");
     Serial.print(bno08x.prodIds.entry[n].swPartNumber);
